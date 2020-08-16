eb0c61760fef20cb961f03255c32c602d8f3d316
hai <pnghai@gmail.com>
2020-08-16 09:09:17 +0700

    add functions

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImgView1: UIImageView!
+    @IBOutlet weak var diceImgView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnRollPressed(_ sender: Any) {
+        let dices = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImgView1.image = dices[Int.random(in: 0...5)]
+        diceImgView2.image = dices[Int.random(in: 0...5)]
+    }
+    
 }
 
